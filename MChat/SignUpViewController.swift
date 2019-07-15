cc16c81cb06a02104de7a74ca76944ef754f2b88
Михаил Колотилин <kolotilinma@gmail.com>
2019-07-16 00:23:06 +0300

    Creating a Registration Scene

---
+++ MChat/SignUpViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SignUpViewController.swift
+//  MChat
+//
+//  Created by Михаил on 16/07/2019.
+//  Copyright © 2019 Mikhail Kolotilin. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+
+}
