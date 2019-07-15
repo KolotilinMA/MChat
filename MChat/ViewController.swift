615235f405ecd848dd30bdd338566384a54c5239
Михаил Колотилин <kolotilinma@gmail.com>
2019-07-15 22:15:29 +0300

    Add Welcome Scene Design Welcome view: 	customize header label

--- MChat/ViewController.swift
+++ MChat/ViewController.swift
@@ -10,11 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var signInFacebookButton: UIButton!
+    @IBOutlet var signInGoogleButton: UIButton!
+    @IBOutlet var createAccountButton: UIButton!
+    @IBOutlet var termsOfServiceLabel: UILabel!
+    @IBOutlet var orLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupUI()
     }
 
 
+    func setupUI() {
+        let title = "Create a new account"
+        let subTitle = "\n\nLorem ipsum dolor sit sdflhlsdf dsdfhjs sdukyf syukyukf frysxhghjsg"
+        
+        let attributedText = NSMutableAttributedString(string: title, attributes:
+            [NSAttributedString.Key.font : UIFont.init(name: "Didot", size: 28)!,
+            NSAttributedString.Key.foregroundColor : UIColor.black
+            ])
+        let attributedSubTitle = NSMutableAttributedString(string: subTitle, attributes:
+            [NSAttributedString.Key.font : UIFont.systemFont(ofSize: 16),
+             NSAttributedString.Key.foregroundColor : UIColor(white: 0, alpha: 0.45)
+            ])
+        
+        attributedText.append(attributedSubTitle)
+        
+        let paragrapStyle = NSMutableParagraphStyle()
+        paragrapStyle.lineSpacing = 5
+        
+        attributedText.addAttribute(NSAttributedString.Key.paragraphStyle, value: paragrapStyle, range: NSMakeRange(0, attributedText.length))
+        
+        titleLabel.numberOfLines = 0
+        titleLabel.attributedText = attributedText
+        
+        
+    }
 }
 
