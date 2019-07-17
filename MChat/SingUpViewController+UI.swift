78eb4cd764cb435d322b6f996b511f6447c02c66
Михаил Колотилин <kolotilinma@gmail.com>
2019-07-17 23:04:48 +0300

    setup avatar image

--- MChat/SingUpViewController+UI.swift
+++ MChat/SingUpViewController+UI.swift
@@ -25,6 +25,17 @@ extension SignUpViewController {
     func setupAvatar() {
         avatarImage.layer.cornerRadius = 40
         avatarImage.clipsToBounds = true
+        avatarImage.isUserInteractionEnabled = true
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(presentPicker))
+        avatarImage.addGestureRecognizer(tapGesture)
+    }
+    
+    @objc func presentPicker() {
+        let picker = UIImagePickerController()
+        picker.sourceType = .photoLibrary
+        picker.allowsEditing = true
+        picker.delegate = self
+        self.present(picker, animated: true, completion: nil)
     }
     
     func setupFullnameTextField() {
@@ -92,4 +103,16 @@ extension SignUpViewController {
         signInButton.setAttributedTitle(attributedText, for: UIControl.State.normal)
     }
     
+}
+
+extension SignUpViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let imageSelected = info[UIImagePickerController.InfoKey.editedImage] as? UIImage {
+            avatarImage.image = imageSelected
+        }
+        if let imageOriginal = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            avatarImage.image = imageOriginal
+        }
+        picker.dismiss(animated: true, completion: nil)
+    }
 }
